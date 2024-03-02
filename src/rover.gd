2a5dde565cea4cc419298453491462a5a0de0d53
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-03-02 07:20:03 -0400

    Add camera limits

--- src/rover.gd
+++ src/rover.gd
@@ -2,6 +2,14 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var level_size : Vector2 = Vector2(1000,1000)
+@onready var camera_2d : Camera2D = $Camera2D
+
+func _ready():
+	# set camera limit to level_size
+	camera_2d.limit_left = 0
+	camera_2d.limit_top = 0
+	camera_2d.limit_right = level_size.x
+	camera_2d.limit_bottom = level_size.y
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -21,4 +29,4 @@ func _process(delta):
 		#$AnimatedSprite2D.stop()
 	
 	position += velocity * delta
-	position = position.clamp(Vector2(16,16), level_size)
+	position = position.clamp(Vector2(16,16), level_size - Vector2(16,16))
