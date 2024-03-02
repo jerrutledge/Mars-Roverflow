99d8fa53676599c4cc67e90666b326bb4f880ea5
noahmacaulay <usernameneeded@gmail.com>
2024-03-02 17:22:34 -0400

    fixed window text box size

--- scenes/TextWindow.gd
+++ scenes/TextWindow.gd
@@ -7,3 +7,18 @@ func _ready():
 
 func _on_close_requested():
 	queue_free()
+	$Text.size = size
+	$ColorRect.size = size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_close_requested():
+	hide()
+
+func _on_size_changed():
+	$Text.size = size
+	$ColorRect.size = size
+	
