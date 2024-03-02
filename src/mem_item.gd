8475c14b7c3589f85528be5af4651745cfa12934
noahmacaulay <usernameneeded@gmail.com>
2024-03-02 14:30:02 -0400

    windows
    added windows which appear when double clicking files

--- src/mem_item.gd
+++ src/mem_item.gd
@@ -1,5 +1,6 @@
 extends ReferenceRect
 @onready var label = $HBoxContainer/Label
+@onready var text_window = preload("res://scenes/TextWindow.tscn")
 
 var hex_chars = "0123456789ABCDEF"
 var data : String = ""
@@ -11,9 +12,8 @@ func _ready():
 		data = data + hex_chars[randi() % 16]
 	label.text = data.substr(0, 20) + "..."
 
-func _on_mouse_entered():
-	pass
-
+var double_click_limit = 200.0
+var last_click_time = 0.0
 
 
 func _input(event):
@@ -22,6 +22,17 @@ func _input(event):
 			position.y < event.position.y and event.position.y < position.y + size.y:
 			selected = true
 			$SelectRect.visible = true
+			var current_time = Time.get_ticks_msec()
+			if current_time - last_click_time < double_click_limit:
+				var manager = get_parent()
+				var desktop = manager.get_parent()
+				var window = text_window.instantiate()
+				window.position = Vector2(global_position.x + 400, global_position.y + 200)
+				desktop.add_child(window)
+				print("Click!")
+			else:
+				last_click_time = current_time
+			
 		else:
 			selected = true
 			$SelectRect.visible = false
