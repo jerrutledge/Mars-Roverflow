bac895279a07b8178075fff780cd1f91bcc02f71
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-03-02 10:04:57 -0400

    Add collectible scene

---
+++ src/MemoryManager.gd
@@ -0,0 +1,9 @@
+extends Panel
+
+@onready var v_box_container = $VBoxContainer
+
+var mem_item_tscn = preload("res://scenes/mem_item.tscn")
+
+func make_new_mem_item():
+	var new_item = mem_item_tscn.instantiate()
+	v_box_container.add_child(new_item)
