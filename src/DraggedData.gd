be2b3ee96de5822f5526ac4e84b1ef5c4377c9ac
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-03-03 13:36:26 -0400

    Make dragged data into a window

--- src/DraggedData.gd
+++ src/DraggedData.gd
@@ -1,24 +1,33 @@
-extends Sprite2D
+extends Window
 
 var mem_item : Node = null
 var held : bool = false
 var init_pos : Vector2 = Vector2.ZERO
+@onready var sprite_2d : Sprite2D = %Sprite2D
+@onready var sprite_size : Vector2 = sprite_2d.texture.get_size()
 
 func _ready():
-	init_pos = get_global_mouse_position()
+	init_pos = Global.get_desktop().get_global_mouse_position()
+	add_theme_icon_override("close", Texture2D.new())
+	mem_item.tree_exited.connect(_on_item_delete)
+	get_viewport().transparent_bg = true
 
 func _process(_delta):
 	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		mem_item.show()
+		if mem_item:
+			mem_item.show()
 		queue_free()
 		return
-	var mousepos = get_global_mouse_position()
+	var mousepos = Global.get_desktop().get_global_mouse_position()
 	if !held:
 		var dist = mousepos - init_pos
 		if dist.length() > 20:
 			held = true
 			show()
-			mem_item.hide()
+			if mem_item:
+				mem_item.hide()
 	else:
-		global_transform.origin = get_global_mouse_position()
-	
+		position = mousepos - (sprite_size / 2)
+
+func _on_item_delete():
+	mem_item = null
