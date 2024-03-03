f131bb515ac215ca8d3467d3716d4e0361bfae14
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-03-03 17:06:15 -0400

    Update DraggedData.gd

--- src/DraggedData.gd
+++ src/DraggedData.gd
@@ -20,7 +20,7 @@ func _process(_delta):
 			return
 		if Global.mouse_on_save:
 			if mem_item:
-				mem_item.queue_free()
+				mem_item.delete_item()
 			if is_valid:
 				Global.score_add(50)
 			else:
